06e2e2587f042e7593c9b8a3a51e563a706ab712
Shweta <101639796+SHWETA420@users.noreply.github.com>
2025-07-23 17:17:17 +0530

    Fix Vite base path and homepage for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/shweta-portfolio/', // 👈 Add this line
+  base: '/my-portfolio/', // 👈 Add this line
   plugins: [react()],
   css: {
     modules: {

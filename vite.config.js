e82354f7b9ce09f8eb872ce8f35f0b690b2794bb
Shweta <101639796+SHWETA420@users.noreply.github.com>
2025-07-24 11:46:47 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shweta-portfolio",
+  base: "/s_portfolio",
   css:{
     modules : {
         localsConvention:"camelCase",
